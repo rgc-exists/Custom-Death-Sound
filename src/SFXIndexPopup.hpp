12ff759b9e94c7adf6fb6cbe18225a67de78e76c
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-06-12 17:43:29 +0100

    add error text + fixed speech marks

--- src/SFXIndexPopup.hpp
+++ src/SFXIndexPopup.hpp
@@ -8,6 +8,7 @@ class SFXIndexPopup : public geode::Popup<>, TableViewCellDelegate {
 private:
     LoadingSpinner* m_loadingCircle;
     deathsounds::Border* m_sfxList;
+    CCLabelBMFont* m_errorText;
 
 protected:
     bool setup() override {
@@ -29,6 +30,11 @@ protected:
         m_sfxList->setPosition({ 50.f, 35.f });
         m_sfxList->setVisible(false);
         m_mainLayer->addChild(m_sfxList);
+
+        m_errorText = CCLabelBMFont::create("", "bigFont.fnt", 350.f);
+        m_errorText->setPosition(m_mainLayer->getContentSize() / 2);
+        m_errorText->setScale(0.5f);
+        m_mainLayer->addChild(m_errorText);
         
         auto refreshButton = CCMenuItemSpriteExtra::create(
             CCSprite::createWithSpriteFrameName("GJ_updateBtn_001.png"),
@@ -45,20 +51,31 @@ protected:
     void showLoading() {
         m_loadingCircle->setVisible(true);
         m_sfxList->setVisible(false);
+        m_errorText->setVisible(false);
     }
 
-    void showResults() {
+    void loadingError(const char* text) {
+        m_loadingCircle->setVisible(false);
+        m_errorText->setString(text);
+        m_errorText->setVisible(true);
+    }
+
+    void showResults(const matjson::Value& result) {
         m_loadingCircle->setVisible(false);
         m_sfxList->setVisible(true);
+        m_errorText->setVisible(false);
     }
 
     void refreshPage(CCObject*) {
         showLoading();
         deathsounds::DSRequest::get()->getTopSFXList([this](const matjson::Value& result) {
-            showResults(); // pass result as param later
+            showResults(result);
         }, [this](const matjson::Value& result) {
-            Notification::create(result.object().parse("error").unwrapOr("Unknown error.").dump(), NotificationIcon::Error)->show();
-            showResults();
+            std::string errorStr = result["error"].dump();
+            if (!errorStr.empty() && errorStr.front() == '"' && errorStr.back() == '"') {
+                errorStr = errorStr.substr(1, errorStr.size() - 2);
+            }
+            loadingError(errorStr.c_str());
         });
     }
 
